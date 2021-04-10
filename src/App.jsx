0b494b501e6788159a98c40854068a4e53d2c95d
Kévin <bdtkevin@gmail.com>
2021-04-10 18:37:27 +0200

    routing system implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
+import All from './components/All';
+import Specs from './components/Specs';
+import AboutUs from './components/AboutUs';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Switch>
+          <Route exact path="/" component={All} />
+          <Route path="/specs" component={Specs} />
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
       </div>
     </Router>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,28 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
-  return <header />;
+  return (
+    <header>
+      <nav>
+        <div className="navLinks">
+          <ul>
+            <li>
+              <Link to="/">All</Link>
+            </li>
+            <li>
+              <Link to="/specs">Specs</Link>
+            </li>
+            <li>
+              <Link to="/about-us">About us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
