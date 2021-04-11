88453605dfd409cd9c74b7027bbd51eff33f13fe
Kévin <bdtkevin@gmail.com>
2021-04-11 12:05:07 +0200

    Add link for go page <all> when click on logod
    add Link logo and change structure menu nav for a better responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,59 +4,62 @@ import './Header.scss';
 
 const Header = () => {
   const [select, setSelect] = useState(1);
-  const [widthScreen, setWidthScreen] = useState(window.innerWidth);
+  const [widthScreen, setWidthScreen] = useState(window.innerW);
   useEffect(() => {
     window.addEventListener('resize', () => setWidthScreen(window.innerWidth));
   }, [widthScreen]);
   return (
     <header>
       <nav>
-        <img
-          className="navLogo"
-          src={`/img/${widthScreen > 800 ? 'logoDesktop' : 'logoPhone'}.svg`}
-          alt="logo"
-        />
-        <ul className="navLinks">
-          <li>
-            <Link
-              onClick={() => setSelect(1)}
-              className={`navLink ${select === 1 && 'selected'}`}
-              to="/"
-            >
-              All
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={() => setSelect(2)}
-              className={`navLink ${select === 2 && 'selected'}`}
-              to="/specs"
-            >
-              Specs
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={() => setSelect(3)}
-              className={`navLink ${select === 3 && 'selected'}`}
-              to="/about-us"
-            >
-              About us
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={() => setSelect(4)}
-              className={`navLink ${select === 4 && 'selected'}`}
-              to="/contact"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-        <button className="rocketButton" type="button">
-          <img src="/img/rocketButtonText.png" alt="rocket mode" />
-        </button>
+        <Link onClick={() => setSelect(1)} className="navLogo" to="/">
+          <img
+            src={`/img/${widthScreen > 800 ? 'logoDesktop' : 'logoPhone'}.svg`}
+            alt="logo"
+          />
+        </Link>
+        <div className="menuContainer">
+          <ul className="navLinks">
+            <li>
+              <Link
+                onClick={() => setSelect(1)}
+                className={`navLink ${select === 1 && 'selected'}`}
+                to="/"
+              >
+                All
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => setSelect(2)}
+                className={`navLink ${select === 2 && 'selected'}`}
+                to="/specs"
+              >
+                Specs
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => setSelect(3)}
+                className={`navLink ${select === 3 && 'selected'}`}
+                to="/about-us"
+              >
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => setSelect(4)}
+                className={`navLink ${select === 4 && 'selected'}`}
+                to="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <button className="rocketButton" type="button">
+            <img src="/img/rocketButtonText.png" alt="rocket mode" />
+          </button>
+        </div>
       </nav>
     </header>
   );
