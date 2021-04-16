c55444e47c1371329082cf3ebe7fd446c0106b9b
AntoineCau <acauwd@gmail.com>
2021-04-16 11:37:55 +0200

    add search bar in footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,17 @@ const Header = () => {
   return (
     <header className="Header">
       <img src="/img/logoDesktop.svg" alt="logo" className="logoDesktop" />
+      <div className="searchContainer">
+        <form className="formSearch" method="get">
+          <input
+            type="search"
+            name="siteSearch"
+            id="searchBar"
+            placeholder="Search by name or number of the pokemon"
+          />
+        </form>
+      </div>
+      <div />
     </header>
   );
 };
