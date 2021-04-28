2750f6e105b91236ee45be6a39c19209c2563e07
Zamomako <robin-delannoy_student2021@wilder.school>
2021-04-28 10:47:00 +0200

    functional searchbar added

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -7,6 +7,11 @@ const PokemonList = () => {
   /* Declaration of a variable with useState to store the return value of the
     call to the API (useSate initialized with an empty array) */
   const [pokemons, setPokemons] = useState([]);
+  const [query, setQuery] = useState('a');
+
+  const handleQueryChange = (e) => {
+    setQuery(e.target.value);
+  };
 
   /* Call the API to get the list of pokémons that we want to display on the
     Homepage (here the 50 first Pokemon) */
@@ -19,14 +24,18 @@ const PokemonList = () => {
   }, []);
 
   return (
-    <ul className="pokemon-list">
-      {/* For each pokemon returned by the API and stored in the useState:
-        "pokemons", call of the <Pokemon /> component with the API url of this
-        one as props to display the Pokemon */}
-      {pokemons.map((pokemon) => {
-        return <Pokemon key={pokemon.name} url={pokemon.url} />;
-      })}
-    </ul>
+    <>
+      <input type="text" value={query} onChange={handleQueryChange} />
+      <ul className="pokemon-list">
+        {pokemons
+          .filter((pokemon) => {
+            return pokemon.name.includes(query);
+          })
+          .map((pokemon) => {
+            return <Pokemon key={pokemon.name} url={pokemon.url} />;
+          })}
+      </ul>
+    </>
   );
 };
 
